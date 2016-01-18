87b59b34bca19c0cb2e9a13767e4b6bc3a1e82bf
enleightond <enleightond@yahoo.com>
2016-01-18 13:24:43 -0800

    setup express generator, localserver

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/index', function(req, res, next) {
-  res.render('index');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Galvanize Reads' });
 });
+
+module.exports = router;
